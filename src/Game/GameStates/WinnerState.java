4f37170bb0173772e35bf40d501cb484039e66ae
Walbert Garcia <walbert.garcia@upr.edu>
2019-09-12 10:25:30 -0400

    Last Commit

--- src/Game/GameStates/WinnerState.java
+++ src/Game/GameStates/WinnerState.java
@@ -7,7 +7,8 @@ import Main.Handler;
 import Resources.Images;
 import UI.UIImageButton;
 import UI.UIManager;
-
+import Game.Entities.Dynamic.Player;
+import Game.Entities.Dynamic.Player2;
 /**
  * Created by AlexVR on 7/1/2018.
  */
@@ -17,7 +18,7 @@ public class WinnerState extends State {
 
 	public WinnerState(Handler handler) {
 		super(handler);
-		//Winnerstate
+		//WinnerState
 		uiManager = new UIManager(handler);
 		handler.getMouseManager().setUimanager(uiManager);
 		//Reset button
@@ -25,19 +26,25 @@ public class WinnerState extends State {
 			handler.getMouseManager().setUimanager(null);
 			handler.getGame().reStart();
 			State.setState(handler.getGame().gameState);
+			Main.GameSetUp.winner1 = false;
+			Main.GameSetUp.winner2 = false;
 
 		}));
-
+		//OptionState
 		uiManager.addObjects(new UIImageButton(handler.getWidth()/3 + 60, handler.getHeight()/2 + 150, 128, 64, Images.Options, () -> {
 			handler.getMouseManager().setUimanager(null);
 			State.setState(handler.getGame().menuState);
+			Main.GameSetUp.winner1 = false;
+			Main.GameSetUp.winner2 = false;
 		}));
-
+		//Title
 		uiManager.addObjects(new UIImageButton(handler.getWidth()/3 + 350, handler.getHeight()/2 + 150, 128, 64, Images.BTitle, () -> {
 			handler.getMouseManager().setUimanager(null);
 			State.setState(handler.getGame().menuState);
+			Main.GameSetUp.winner1 = false;
+			Main.GameSetUp.winner2 = false;
 		}));
-
+		
 	}
 
 	@Override
@@ -53,8 +60,10 @@ public class WinnerState extends State {
 		//Verifies who the winner is.
 		if(Main.GameSetUp.winner1 == true) {
 			g.drawImage(Images.Winner1,0,0,800,600,null);
+			
 		}else if (Main.GameSetUp.winner2 == true){
 			g.drawImage(Images.Winner2,0,0,800,600,null);
+			
 		}
         uiManager.Render(g);
 	}
